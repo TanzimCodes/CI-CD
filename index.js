3efe0f47296b09d318610180f7697a1418db8494
tanzim <tanzim@softwaredeveloper.com>
2024-07-11 16:58:21 -0500

    This should deploy to heroku

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello, CI/CI")
+const express = require('express');
+const app = express();
+const port = 3000;
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to my Express app!');
+});
+
+app.post('/api/data', (req, res) => {
+  const { name, email } = req.body;
+  res.send(`Received data: ${name} ${email}`);
+});
+
+// Start server
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
+
